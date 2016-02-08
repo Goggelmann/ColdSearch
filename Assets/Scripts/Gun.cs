73a540ba7b9901d42cafac27bd3b3ff16770ebe1
Oliver Boots <Oliver.boots@hotmail.com>
2016-02-08 14:05:06 +0100

    no message

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public GameObject bullet;
+	public float velocity = 20f;
+	public float rateOfFire = 0.5f;
+
+	bool canShoot;
+
+
+	//
+	IEnumerator Reload(float delay)
+	{
+		yield return new WaitForSeconds (delay);
+		canShoot = true;
+	}
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Reload (rateOfFire));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)&& canShoot)
+		{
+			GameObject tempBullet = Instantiate (bullet, transform.position, transform.rotation) as GameObject;
+			Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+			Vector3 dir = (Input.mousePosition + sp).normalized;
+			tempBullet.GetComponent<Rigidbody2D>().velocity = (gameObject.transform.right * velocity);
+			canShoot = false;
+			StartCoroutine (Reload (rateOfFire));
+		}
+	}
+}
---
+++ Assets/ShootStart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class ShootStart : MonoBehaviour {
+
+	public float velocity = 20f;
+	// Use this for initialization
+	void Start () 
+	{
+		Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+		Vector3 dir = (Input.mousePosition - sp).normalized;
+		GetComponent<Rigidbody>().AddForce (gameObject.transform.forward * velocity);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
